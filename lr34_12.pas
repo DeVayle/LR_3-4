2120ff15d66d8aede721b9785d6db4233f44603a
DeVayle <144678017+DeVayle@users.noreply.github.com>
2023-10-04 11:58:26 +0300

    Add files via upload

---
+++ lr34_12.pas
@@ -0,0 +1,18 @@
+﻿program lr34_12;
+
+var
+  a, b, i, s, p: integer;
+  
+begin
+  writeln ('Введите диапазон чисел от a до b');
+  readln (a, b);
+  s := 0;
+  p := 1;
+  while a <= b do
+  begin
+    if a mod 2 = 0 then p:=p*a
+      else s:=s+a;
+    a := a+1;
+  end;
+  writeln ('Сумма нечетных чисел = ', s, ', Произведение четных чисел = ', p)
+end.
